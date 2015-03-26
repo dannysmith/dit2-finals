e5e6f9dbdc63cb04cc0d84645993ee1b6fc4876d
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-03-26 15:28:02 +0000

    created the steps file for creating a course with the template given from cucumber

---
+++ features/step_definitions/create_course_steps.rb
@@ -0,0 +1,35 @@
+Given(/^I am logged in as a teacher$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I create a new course with valid data$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be able to see it on my list of courses$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it should be able to be seen by a student$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I try to create a new course without filling out all the required information$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it should not allow me to create the course$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^should inform me of what extra information is needed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am logged in as a student$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it should not be possible for me to create a new course$/) do
+  pending # express the regexp above with the code you wish you had
+end
