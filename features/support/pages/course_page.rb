c95933eebcac30303976631f564a7ee8b860dfea
Eric Darko <edarko@spartaglobal.com>
2015-03-26 17:53:50 +0000

    changed search method name

--- features/support/pages/course_page.rb
+++ features/support/pages/course_page.rb
@@ -3,7 +3,7 @@ class CoursePage < GenericPage
     @browser.goto "http://unix.spartaglobal.com/moodle/course/index.php"
   end
 
-  def search=(search_term)
+  def search_for(search_term)
     @browser.text_field(id: "navsearchbox").send_keys search_term
     @browser.input(value: "Go").click
   end
