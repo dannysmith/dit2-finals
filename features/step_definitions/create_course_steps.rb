7b62735144049d7a7ee989dadd378b0f6eecebbd
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-03-26 17:16:02 +0000

    added negative test for no fields filled in when creating a course

--- features/step_definitions/create_course_steps.rb
+++ features/step_definitions/create_course_steps.rb
@@ -1,4 +1,5 @@
 Given(/^I am logged in as a teacher$/) do
+  binding.pry
   pending # express the regexp above with the code you wish you had
 end
 
@@ -19,11 +20,13 @@ When(/^I create a new course with invalid data, missing out required information
 end
 
 Then(/^it should not allow me to create the course$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(browser.url).to eq('http://unix.spartaglobal.com/moodle/course/request.php')
 end
 
 Then(/^should inform me of what extra information is needed$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(browser.div(id: 'fitem_id_fullname').span(class: 'error').text).to eq('Missing full name')
+  expect(browser.div(id: 'fitem_id_shortname').span(class: 'error').text).to eq('Missing short name')
+  expect(browser.div(id: 'fitem_id_reason').span(class: 'error').text).to eq('Missing reason')
 end
 
 Given(/^I am logged in as a student$/) do
