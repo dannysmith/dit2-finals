28a349ea66f54def52d8eed5be850c8033d0c232
Eric Darko <edarko@spartaglobal.com>
2015-03-26 16:02:31 +0000

    Added dirty steps folder

---
+++ features/step_definitions/dirty_steps/course_search_steps.steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am login as a student$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I search for Biology$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a list of results for this course$/) do
+  pending # express the regexp above with the code you wish you had
+end
