eb17665e897bd01831ef8b24a5df9142d8cd2f68
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-03-26 14:34:15 +0000

    DITA-9 created feature file for the course search

---
+++ features/step_definitions/search_courses_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am logged in as a student,$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter 'Biology' as a course search term$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^courses relating to Biology should be shown$/) do
+  pending # express the regexp above with the code you wish you had
+end
