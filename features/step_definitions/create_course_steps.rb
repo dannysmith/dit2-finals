6044cf1298cf81f80be48096453024de9ec8cafc
Richard Twena <Richardtwena@gmail.com>
2015-03-27 15:00:07 +0000

    removed uneeded steps, added nagtive step code

--- features/step_definitions/create_course_steps.rb
+++ features/step_definitions/create_course_steps.rb
@@ -25,32 +25,17 @@ Then(/^I should be able to see it on my list of courses$/) do
   @app.course_search_page.check_results 'Software Engineering'
 end
 
-Then(/^it should be able to be seen by a student$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
 When(/^I create a new course with invalid data, missing out required information$/) do
-  pending # express the regexp above with the code you wish you had
-  # browser.goto 'http://unix.spartaglobal.com/moodle/course/request.php'
-  # browser.button(text: 'Request a course').click
+  @browser.goto 'http://unix.spartaglobal.com/moodle/course/request.php'
+  @browser.button(text: 'Request a course').click
 end
 
 Then(/^it should not allow me to create the course$/) do
-  pending # express the regexp above with the code you wish you had
-  # expect(@browser.url).to eq('http://unix.spartaglobal.com/moodle/course/request.php')
+  expect(@browser.url).to eq('http://unix.spartaglobal.com/moodle/course/request.php')
 end
 
 Then(/^should inform me of what extra information is needed$/) do
-  pending # express the regexp above with the code you wish you had
-  # expect(@browser.div(id: 'fitem_id_fullname').span(class: 'error').text).to eq('Missing full name')
-  # expect(@browser.div(id: 'fitem_id_shortname').span(class: 'error').text).to eq('Missing short name')
-  # expect(@browser.div(id: 'fitem_id_reason').span(class: 'error').text).to eq('Missing reason')
-end
-
-Given(/^I am logged in as a student$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^it should not be possible for me to create a new course$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@browser.div(id: 'fitem_id_fullname').span(class: 'error').text).to eq('Missing full name')
+  expect(@browser.div(id: 'fitem_id_shortname').span(class: 'error').text).to eq('Missing short name')
+  expect(@browser.div(id: 'fitem_id_reason').span(class: 'error').text).to eq('Missing reason')
 end
