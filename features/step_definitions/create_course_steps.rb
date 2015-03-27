330bacbc879a86bb6ce2fd32929b5b1b5be11b7a
Richard Twena <Richardtwena@gmail.com>
2015-03-27 11:44:45 +0000

    DITA-16 finished login as teacher, finished creating a course with valid data and marked steps as pending

--- features/step_definitions/create_course_steps.rb
+++ features/step_definitions/create_course_steps.rb
@@ -4,14 +4,13 @@ Given(/^I am logged in as a teacher$/) do
 end
 
 When(/^I create a new course with valid data$/) do
-  @browser.goto 'http://unix.spartaglobal.com/moodle/course/'
-  @browser.button(value: 'Request a course').click
-  @browser.text_field(id: 'id_fullname').set 'Software Enginering'
-  @browser.text_field(id: 'id_shortname').set 'SftEng'
-  @browser.element(id: 'id_summary_editoreditable').send_keys 'This course will take you through the wonders of software engineering'
-  @browser.text_field(id: 'id_reason').set 'Reason message'
-  @browser.button(id: 'id_submitbutton').click
-  raise "Possibly missing required information" unless expect(browser.element(id: 'notice').text).to include 'Your course request has been saved successfully'
+  @app.course_request_page.visit
+  @app.course_request_page.fill_form 'Software Enginering1234', 'SftEng1243', 'This course will take you through the wonders of software engineering', 'Reason message'
+  raise "Possibly missing required information" unless @browser.element(id: 'notice').text.include?('Your course request has been saved successfully')
+end
+
+When(/^the course is approved by an administrator$/) do
+
 end
 
 Then(/^I should be able to see it on my list of courses$/) do
@@ -23,18 +22,21 @@ Then(/^it should be able to be seen by a student$/) do
 end
 
 When(/^I create a new course with invalid data, missing out required information$/) do
-  browser.goto 'http://unix.spartaglobal.com/moodle/course/request.php'
-  browser.button(text: 'Request a course').click
+  pending # express the regexp above with the code you wish you had
+  # browser.goto 'http://unix.spartaglobal.com/moodle/course/request.php'
+  # browser.button(text: 'Request a course').click
 end
 
 Then(/^it should not allow me to create the course$/) do
-  expect(browser.url).to eq('http://unix.spartaglobal.com/moodle/course/request.php')
+  pending # express the regexp above with the code you wish you had
+  # expect(@browser.url).to eq('http://unix.spartaglobal.com/moodle/course/request.php')
 end
 
 Then(/^should inform me of what extra information is needed$/) do
-  expect(browser.div(id: 'fitem_id_fullname').span(class: 'error').text).to eq('Missing full name')
-  expect(browser.div(id: 'fitem_id_shortname').span(class: 'error').text).to eq('Missing short name')
-  expect(browser.div(id: 'fitem_id_reason').span(class: 'error').text).to eq('Missing reason')
+  pending # express the regexp above with the code you wish you had
+  # expect(@browser.div(id: 'fitem_id_fullname').span(class: 'error').text).to eq('Missing full name')
+  # expect(@browser.div(id: 'fitem_id_shortname').span(class: 'error').text).to eq('Missing short name')
+  # expect(@browser.div(id: 'fitem_id_reason').span(class: 'error').text).to eq('Missing reason')
 end
 
 Given(/^I am logged in as a student$/) do
