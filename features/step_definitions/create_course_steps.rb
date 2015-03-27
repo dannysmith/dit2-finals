8039801287db8c44a4392b2e8a80a1f1a30864be
Richard Twena <Richardtwena@gmail.com>
2015-03-27 13:53:19 +0000

    added step code for approving a course and checking to if the course exists

--- features/step_definitions/create_course_steps.rb
+++ features/step_definitions/create_course_steps.rb
@@ -5,16 +5,24 @@ end
 
 When(/^I create a new course with valid data$/) do
   @app.course_request_page.visit
-  @app.course_request_page.fill_form 'Software Enginering1234', 'SftEng1243', 'This course will take you through the wonders of software engineering', 'Reason message'
+  @app.course_request_page.fill_form 'Software Engineering', 'SftEng', 'This course will take you through the wonders of software engineering', 'Reason message'
   raise "Possibly missing required information" unless @browser.element(id: 'notice').text.include?('Your course request has been saved successfully')
 end
 
 When(/^the course is approved by an administrator$/) do
-
+  @app.logout
+  @app.login.visit
+  @app.login.admin_login
+  @app.course_pending.visit
+  @app.course_pending.approve
 end
 
 Then(/^I should be able to see it on my list of courses$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.logout
+  @app.login.visit
+  @app.login.login 'spartatest1', 'Spartatest1!'
+  @app.course_search_page.visit 'Software Engineering'
+  @app.course_search_page.check_results 'Software Engineering'
 end
 
 Then(/^it should be able to be seen by a student$/) do
