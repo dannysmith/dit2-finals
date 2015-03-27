b8a2ad05df76575239f9a0f4ca6c35fd9ef93cce
Richard Twena <Richardtwena@gmail.com>
2015-03-27 15:50:05 +0000

    DITA-16 reworked valid/invalid course steps into one step, added steps for nagnegative test scenario

--- features/step_definitions/create_course_steps.rb
+++ features/step_definitions/create_course_steps.rb
@@ -3,10 +3,14 @@ Given(/^I am logged in as a teacher$/) do
   @app.login.login 'spartatest1', 'Spartatest1!'
 end
 
-When(/^I create a new course with valid data$/) do
+When(/^I create a new course with (valid|invalid) data/) do |status|
   @app.course_request_page.visit
-  @app.course_request_page.fill_form 'Software Engineering', 'SftEng', 'This course will take you through the wonders of software engineering', 'Reason message'
-  raise "Possibly missing required information" unless @browser.element(id: 'notice').text.include?('Your course request has been saved successfully')
+  if status == 'valid'
+    @app.course_request_page.fill_form 'Software Engineering', 'SftEng', 'This course will take you through the wonders of software engineering', 'Reason message'
+    raise "Possibly missing required information" unless @browser.element(id: 'notice').text.include?('Your course request has been saved successfully')
+  else
+    @app.course_request_page.submit
+  end  
 end
 
 When(/^the course is approved by an administrator$/) do
@@ -25,17 +29,10 @@ Then(/^I should be able to see it on my list of courses$/) do
   @app.course_search_page.check_results 'Software Engineering'
 end
 
-When(/^I create a new course with invalid data, missing out required information$/) do
-  @browser.goto 'http://unix.spartaglobal.com/moodle/course/request.php'
-  @browser.button(text: 'Request a course').click
-end
-
 Then(/^it should not allow me to create the course$/) do
-  expect(@browser.url).to eq('http://unix.spartaglobal.com/moodle/course/request.php')
+  expect(@browser.url).to eq(@app.course_request_page.url)
 end
 
 Then(/^should inform me of what extra information is needed$/) do
-  expect(@browser.div(id: 'fitem_id_fullname').span(class: 'error').text).to eq('Missing full name')
-  expect(@browser.div(id: 'fitem_id_shortname').span(class: 'error').text).to eq('Missing short name')
-  expect(@browser.div(id: 'fitem_id_reason').span(class: 'error').text).to eq('Missing reason')
+  @app.course_request_page.expect_errors 'Missing full name', 'Missing short name', 'Missing reason'
 end
