12bbeb2e7ef7a0fc06ca70f42816a8d43c304fc9
njewootah <njewootah@spartaglobal.com>
2015-03-31 18:07:29 +0100

    Implemented check details method

---
+++ features/support/pages/calendar_day_page.rb
@@ -0,0 +1,14 @@
+class CalendarDayPage < GenericPage
+  def check_details(event_name, description)
+    self.check_description = description
+    self.check_event_name = event_name
+  end
+
+  def check_description=(description)
+    @browser.p(text: description).visible?
+  end
+
+  def check_event_name=(event_name)
+    @browser.div(text: event_name).visible?
+  end
+end
