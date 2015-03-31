981a0e237a757c69a420621dbafda1d35a9523cf
njewootah <njewootah@spartaglobal.com>
2015-03-31 16:24:54 +0100

    DITA-38 Added pending steps

---
+++ features/step_definitions/dirty_steps/create_global_event_steps.rb
@@ -0,0 +1,27 @@
+Given(/^I am logged in as admin$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am on the new event page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I set the type of event to Site$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill in the event details correctly$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the global event on the Calendar page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill in the event details incorrectly$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be prompted with an error message$/) do
+  pending # express the regexp above with the code you wish you had
+end
