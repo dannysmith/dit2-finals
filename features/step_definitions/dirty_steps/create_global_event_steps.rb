da73d6debf9ed5f9e5bb98d8e67c374401b950be
njewootah <njewootah@spartaglobal.com>
2015-03-31 16:40:08 +0100

    Added step methods

--- features/step_definitions/dirty_steps/create_global_event_steps.rb
+++ features/step_definitions/dirty_steps/create_global_event_steps.rb
@@ -1,27 +1,25 @@
 Given(/^I am logged in as admin$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.login.visit
+  @app.login.admin_login
 end
 
 When(/^I am on the new event page$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.new_event.visit
 end
 
-When(/^I set the type of event to Site$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^I set the type of event to (.+)$/) do |type|
+  @app.new_event.event_type = type
 end
 
-When(/^I fill in the event details correctly$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^I fill in the event details (correctly|incorrectly)$/) do |status|
+  @app.new_event.fill_form status
 end
 
 Then(/^I should see the global event on the Calendar page$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I fill in the event details incorrectly$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.calendar.visit
+  @app.calendar.check_event
 end
 
 Then(/^I should be prompted with an error message$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.new_event.check_error
 end
