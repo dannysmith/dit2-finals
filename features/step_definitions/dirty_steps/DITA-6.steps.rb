83e99fe5c4b642893fef4141feef4a37dba706d2
Mohammed Kasso <mkasso@spartaglobal.com>
2015-04-01 16:21:52 +0100

    Added constants to be used in before, after and the step definations, Completed the step definations (requires refactoring)

---
+++ features/step_definitions/dirty_steps/DITA-6.steps.rb
@@ -0,0 +1,49 @@
+COURSE_NAME = 'ITA'
+SHORTNAME = 'ITA'
+SUMMARY = 'IT ITA'
+REASON = 'REASON ITA MESSAGE'
+NUM_OF_USERS = 2
+USERNAME1 = 'bob'
+PASSWORD1 = '12345678aB!'
+EMAIL1 = 'bobharris@sharklasers.com'
+EMAIL21 = 'bobharris@sharklasers.com'
+FIRSTNAME1 = 'Bob'
+LASTNAME1 = 'Harris'
+USERNAME2 = 'kate'
+PASSWORD2 = '12345678aB!'
+EMAIL2 = 'katejohnson@sharklasers.com'
+EMAIL22 = 'katejohnson@sharklasers.com'
+FIRSTNAME2 = 'Kate'
+LASTNAME2 = 'Johnson'
+URL = 'http://unix.spartaglobal.com/moodle/course/index.php?categoryid=1'
+
+Given(/^I am signed in as a teacher$/) do
+  @app.login.visit
+  sleep(5)
+  @app.login.login USERNAME1, PASSWORD1
+end
+
+When(/^I enrol Kate Johnson into the course as a teacher$/) do
+  @browser.goto URL
+  @browser.a(text:COURSE_NAME).click
+  @browser.span(text: "Users").click
+  @browser.a(text: "Enrolled users").click
+  @browser.button(value: "Enrol users").click
+  @browser.select_list(id:"id_enrol_manual_assignable_roles").select("Teacher")
+  @browser.divs(class: 'user').each do |div|
+    if div.div(class: 'fullname').text == (FIRSTNAME2)+' '+(LASTNAME2)
+      div.button(class: 'enrol').click
+      break
+    end
+  end
+  @browser.button(value: "Finish enrolling users").click
+end
+
+Then(/^I should see Kate Johnson in the course as a teacher$/) do
+  @browser.trs(class: 'userinforow').each do |tr|
+    if tr.div(class: 'subfield subfield_firstname').text == (FIRSTNAME2)+' '+(LASTNAME2)
+      expect(tr.div(class: "role role_3").text).to eq('Teacher')
+      break
+    end
+  end
+end
