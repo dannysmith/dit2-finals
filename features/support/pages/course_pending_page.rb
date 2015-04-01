750c934b1745dcd7dfd7659817fa24e43219f1d8
Richard Twena <Richardtwena@gmail.com>
2015-04-01 09:32:18 +0100

    removed whitespace

--- features/support/pages/course_pending_page.rb
+++ features/support/pages/course_pending_page.rb
@@ -7,7 +7,7 @@ class CoursePendingPage < GenericPage
     @browser.goto EnvConfig.course_pending_url
   end
 
-  def approve (course_name)
+  def approve(course_name)
     @browser.tbody.trs.each do |row|
       if row.tds[1].text == course_name
         row.input(ELEMENT[:approve]).click
