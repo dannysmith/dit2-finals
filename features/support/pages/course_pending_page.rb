96f4ed877fe11a23260d3b7a2c205ff0d3f73e4f
Richard Twena <Richardtwena@gmail.com>
2015-04-01 09:27:10 +0100

    DITA-16 fixed approving a course

--- features/support/pages/course_pending_page.rb
+++ features/support/pages/course_pending_page.rb
@@ -7,7 +7,12 @@ class CoursePendingPage < GenericPage
     @browser.goto EnvConfig.course_pending_url
   end
 
-  def approve
-    @browser.input(ELEMENT[:approve]).click
+  def approve (course_name)
+    @browser.tbody.trs.each do |row|
+      if row.tds[1].text == course_name
+        row.input(ELEMENT[:approve]).click
+        break
+      end
+    end
   end
 end
