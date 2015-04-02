a01caa341b275c1458ad257aa8e35a63e6de8114
Mohammed Kasso <mkasso@spartaglobal.com>
2015-04-02 15:53:56 +0100

    Removed all commented lines and changed constants to correct values sto use in the method

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -118,82 +118,15 @@ def setup_enrollment(enroll_user, enroll_type, course)
 end
 
 Before ('@DITA6_setup') do
-  #@app.login.visit
-  #@app.login.admin_login
   user1 = {username:'bob', password:'12345678aB!', email:'bobharris@sharklasers.com', firstname:'Bob', lastname:'Harris'}
   user2  = {username: 'kate', password:'12345678aB!', email:'katejohnson@sharklasers.com', firstname:'Kate', lastname:'Johnson'}
   user_setup ({user1: user1, user2: user2})
-  # @app.signup.visit
-  # @app.signup.username.set USERNAME1
-  # @app.signup.password.set PASSWORD1
-  # @app.signup.email.set EMAIL1
-  # @app.signup.email2.set EMAIL21
-  # @app.signup.firstname.set FIRSTNAME1
-  # @app.signup.lastname.set LASTNAME1
-  # @app.signup.submit.click
-
-  # if (NUM_OF_USERS == 2)
-    # user_setup ({username: 'kate', password:'12345678aB!', email:'katejohnson@sharklasers.com', firstname:'Kate', lastname:'Johnson'})
-    # @app.signup.visit
-    # @app.signup.username.set USERNAME2
-    # @app.signup.password.set PASSWORD2
-    # @app.signup.email.set EMAIL2
-    # @app.signup.email2.set EMAIL22
-    # @app.signup.firstname.set FIRSTNAME2
-    # @app.signup.lastname.set LASTNAME2
-    # @app.signup.submit.click
-  # end
-  
-  # if (NUM_OF_USERS == 2)
-  #   @app.tp_email.visit
-  #   @app.tp_email.account (EMAIL2)[/([^@]+)/]
-  #   sleep(3)
-  #   @app.tp_email.first_li.click
-  #   sleep(3)
-  #   @browser.goto @app.tp_email.email_body.text[/http.+#{USERNAME2}/]
-  # end
-
-  # @app.tp_email.visit
-  # @app.tp_email.account (EMAIL1)[/([^@]+)/]
-  # sleep(3)
-  # @app.tp_email.first_li.click
-  # sleep(3)
-  # @browser.goto @app.tp_email.email_body.text[/http.+#{USERNAME1}/]
   @app.login.visit
   @app.login.login 'bob', '12345678aB!'
   course_setup ({course1:{fullname: 'ITA', shortname: 'ITA', summary: 'IT ITA', reason: 'REASON ITA MESSAGE'}})
   @app.login.visit
   @app.login.admin_login
-  setup_enrollment((FIRSTNAME1)+' '+(LASTNAME1), enroll_type, {fullname: 'ITA'})
-  binding.pry
-  # @app.login.admin_login
-  # @app.course_request_page.visit 
-  # @app.course_request_page.fill_form fullname: COURSE_NAME, shortname: SHORTNAME, summary: SUMMARY, reason: REASON
-  # @browser.a(title:(FIRSTNAME1)+' '+(LASTNAME1)).click
-  # @browser.a(title:'Log out').click
-  # @app.login.visit
-  # @app.login.admin_login
-  # @browser.goto 'http://unix.spartaglobal.com/moodle/course/pending.php'
-  # @browser.tbody.trs.each do |row|
-  #   if row.tds[1].text == COURSE_NAME
-  #     row.input(value: 'Approve').click
-  #     break
-  #   end
-  # end
-
-  # @browser.goto 'http://unix.spartaglobal.com/moodle/course/management.php?categoryid=1'
-  # @browser.a(text: COURSE_NAME).click
-  # @browser.a(text: "Enrolled users").click
-  # @browser.button(value: "Enrol users").click
-  # @browser.select_list(id:"id_enrol_manual_assignable_roles").select("Teacher")
-  # @browser.divs(class: 'user').each do |div|
-  #   if div.div(class: 'fullname').text == (FIRSTNAME1)+' '+(LASTNAME1)
-  #     div.button(class: 'enrol').click
-  #     break
-  #   end
-  # end
-  # @browser.a(title:'Admin User').click
-  # @browser.a(title:'Log out').click
+  setup_enrollment('Bob Harris', 'Teacher', {fullname: 'ITA'})
 end
 
 After ('@DITA6_teardown') do
@@ -201,49 +134,22 @@ After ('@DITA6_teardown') do
   @app.login.admin_login
   @browser.goto EnvConfig.course_manage_url 
   @browser.as(class: 'coursename').each_with_index do |course, i|
-    if course.text == COURSE_NAME
+    if course.text == 'ITA'
       @browser.imgs(alt: 'Delete')[i].click
       @browser.input(value: 'Continue').click
       break
     end
   end
   @browser.goto EnvConfig.modify_users_url
-  if (NUM_OF_USERS == 2)
-    @browser.option(text:(FIRSTNAME2)+' '+(LASTNAME2)).select
-    @browser.button(id:'id_addsel').click
-  end
-  @browser.option(text:(FIRSTNAME1)+' '+(LASTNAME1)).select
+  @browser.option(text:'Kate Johnson').select
+  @browser.button(id:'id_addsel').click
+  @browser.option(text:'Bob Harris').select
   @browser.button(id:'id_addsel').click
   @browser.option(text:'Delete').select
   @browser.button(id:'id_doaction').click
   @browser.button(value:'Yes').click
   @browser.button(value:'Continue').click    
 end
-
-# After ('@course_teardown') do
-#   @app.login.visit
-#   @app.login.admin_login
-
-#   @browser.goto EnvConfig.course_manage_url 
-#   @browser.as(class: 'coursename').each_with_index do |course, i|
-#     if course.text == COURSE_NAME
-#       @browser.imgs(alt: 'Delete')[i].click
-#       @browser.input(value: 'Continue').click
-#       break
-#     end
-#   end
-#   @browser.goto EnvConfig.modify_users_url
-#   if (NUM_OF_USERS == 2)
-#     @browser.option(text:(FIRSTNAME2)+' '+(LASTNAME2)).select
-#     @browser.button(id:'id_addsel').click
-#   end
-#   @browser.option(text:(FIRSTNAME1)+' '+(LASTNAME1)).select
-#   @browser.button(id:'id_addsel').click
-#   @browser.option(text:'Delete').select
-#   @browser.button(id:'id_doaction').click
-#   @browser.button(value:'Yes').click
-#   @browser.button(value:'Continue').click    
-# end
 
 After ('@course_teardown') do
   @app.login.visit
