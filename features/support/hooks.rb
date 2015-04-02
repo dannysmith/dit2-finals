bb8452b38f0f91309030db05945ceede66de3360
Mohammed Kasso <mkasso@spartaglobal.com>
2015-04-02 11:53:30 +0100

    Refactored before and after hook for DITA6

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -110,69 +110,78 @@ end
 Before ('@DITA6_setup') do
   #@app.login.visit
   #@app.login.admin_login
-
-  @app.signup.visit
-  @app.signup.username.set USERNAME1
-  @app.signup.password.set PASSWORD1
-  @app.signup.email.set EMAIL1
-  @app.signup.email2.set EMAIL21
-  @app.signup.firstname.set FIRSTNAME1
-  @app.signup.lastname.set LASTNAME1
-  @app.signup.submit.click
+  user_setup {username: 'bob', password:'12345678aB!', email:'bobharris@sharklasers.com', firstname:'Bob', lastname:'Harris'}
+  # @app.signup.visit
+  # @app.signup.username.set USERNAME1
+  # @app.signup.password.set PASSWORD1
+  # @app.signup.email.set EMAIL1
+  # @app.signup.email2.set EMAIL21
+  # @app.signup.firstname.set FIRSTNAME1
+  # @app.signup.lastname.set LASTNAME1
+  # @app.signup.submit.click
 
   if (NUM_OF_USERS == 2)
-    @app.signup.visit
-    @app.signup.username.set USERNAME2
-    @app.signup.password.set PASSWORD2
-    @app.signup.email.set EMAIL2
-    @app.signup.email2.set EMAIL22
-    @app.signup.firstname.set FIRSTNAME2
-    @app.signup.lastname.set LASTNAME2
-    @app.signup.submit.click
+    user_setup {username: 'kate', password:'12345678aB!', email:'katejohnson@sharklasers.com', firstname:'Kate', lastname:'Johnson'}
+    # @app.signup.visit
+    # @app.signup.username.set USERNAME2
+    # @app.signup.password.set PASSWORD2
+    # @app.signup.email.set EMAIL2
+    # @app.signup.email2.set EMAIL22
+    # @app.signup.firstname.set FIRSTNAME2
+    # @app.signup.lastname.set LASTNAME2
+    # @app.signup.submit.click
   end
   
-  if (NUM_OF_USERS == 2)
-    @app.tp_email.visit
-    @app.tp_email.account (EMAIL2)[/([^@]+)/]
-    sleep(3)
-    @app.tp_email.first_li.click
-    sleep(3)
-    @browser.goto @app.tp_email.email_body.text[/http.+#{USERNAME2}/]
-  end
+  # if (NUM_OF_USERS == 2)
+  #   @app.tp_email.visit
+  #   @app.tp_email.account (EMAIL2)[/([^@]+)/]
+  #   sleep(3)
+  #   @app.tp_email.first_li.click
+  #   sleep(3)
+  #   @browser.goto @app.tp_email.email_body.text[/http.+#{USERNAME2}/]
+  # end
 
-  @app.tp_email.visit
-  @app.tp_email.account (EMAIL1)[/([^@]+)/]
-  sleep(3)
-  @app.tp_email.first_li.click
-  sleep(3)
-  @browser.goto @app.tp_email.email_body.text[/http.+#{USERNAME1}/]
-
-  @app.course_request_page.visit 
-  @app.course_request_page.fill_form fullname: COURSE_NAME, shortname: SHORTNAME, summary: SUMMARY, reason: REASON
-  @browser.a(title:(FIRSTNAME1)+' '+(LASTNAME1)).click
-  @browser.a(title:'Log out').click
+  # @app.tp_email.visit
+  # @app.tp_email.account (EMAIL1)[/([^@]+)/]
+  # sleep(3)
+  # @app.tp_email.first_li.click
+  # sleep(3)
+  # @browser.goto @app.tp_email.email_body.text[/http.+#{USERNAME1}/]
+  eventually{ @app.login.login 'bob', '12345678aB!' }
+  course_setup {fullname: 'ITA', shortname: 'ITA', summary: 'IT ITA', reason: 'REASON ITA MESSAGE'}
+  @app.logout
   @app.login.visit
-  @app.login.admin_login
-  @browser.goto 'http://unix.spartaglobal.com/moodle/course/pending.php'
-  @browser.tbody.trs.each do |row|
-    if row.tds[1].text == COURSE_NAME
-      row.input(value: 'Approve').click
-      break
-    end
-  end
-  @browser.goto 'http://unix.spartaglobal.com/moodle/course/management.php?categoryid=1'
-  @browser.a(text: COURSE_NAME).click
-  @browser.a(text: "Enrolled users").click
-  @browser.button(value: "Enrol users").click
-  @browser.select_list(id:"id_enrol_manual_assignable_roles").select("Teacher")
-  @browser.divs(class: 'user').each do |div|
-    if div.div(class: 'fullname').text == (FIRSTNAME1)+' '+(LASTNAME1)
-      div.button(class: 'enrol').click
-      break
-    end
-  end
-  @browser.a(title:'Admin User').click
-  @browser.a(title:'Log out').click
+  eventually { @app.login.admin_login }
+  setup_enrollment((FIRSTNAME1)+' '+(LASTNAME1), enroll_type, {fullname: 'ITA'})
+  @app.logout
+  # @app.login.admin_login
+  # @app.course_request_page.visit 
+  # @app.course_request_page.fill_form fullname: COURSE_NAME, shortname: SHORTNAME, summary: SUMMARY, reason: REASON
+  # @browser.a(title:(FIRSTNAME1)+' '+(LASTNAME1)).click
+  # @browser.a(title:'Log out').click
+  # @app.login.visit
+  # @app.login.admin_login
+  # @browser.goto 'http://unix.spartaglobal.com/moodle/course/pending.php'
+  # @browser.tbody.trs.each do |row|
+  #   if row.tds[1].text == COURSE_NAME
+  #     row.input(value: 'Approve').click
+  #     break
+  #   end
+  # end
+
+  # @browser.goto 'http://unix.spartaglobal.com/moodle/course/management.php?categoryid=1'
+  # @browser.a(text: COURSE_NAME).click
+  # @browser.a(text: "Enrolled users").click
+  # @browser.button(value: "Enrol users").click
+  # @browser.select_list(id:"id_enrol_manual_assignable_roles").select("Teacher")
+  # @browser.divs(class: 'user').each do |div|
+  #   if div.div(class: 'fullname').text == (FIRSTNAME1)+' '+(LASTNAME1)
+  #     div.button(class: 'enrol').click
+  #     break
+  #   end
+  # end
+  # @browser.a(title:'Admin User').click
+  # @browser.a(title:'Log out').click
 end
 
 After ('@DITA6_teardown') do
