10d5e07b1204547c8b83a51442118afc345203e5
njewootah <njewootah@spartaglobal.com>
2015-04-07 09:01:03 +0100

    Deleted create group method

--- features/support/pages/group_page.rb
+++ features/support/pages/group_page.rb
@@ -1,7 +1,4 @@
 class GroupPage < GenericPage
-  def select_create_group
-    @browser.input(value: "Create group").click
-  end
   def select_add_members
     @browser.input(id: "showaddmembersform").click
   end
