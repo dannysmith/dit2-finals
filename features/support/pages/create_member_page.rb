1f651fd8ffe77e4d8258bbe15885a8fca47741e0
njewootah <njewootah@spartaglobal.com>
2015-04-07 09:00:16 +0100

    Renamed method name to submit_form

--- features/support/pages/create_member_page.rb
+++ features/support/pages/create_member_page.rb
@@ -1,5 +1,5 @@
 class CreateMemberPage < GenericPage
-  def add_members(user1, user2)
+  def submit_form(user1, user2)
     @browser.option(text: /^#{user1}+/).select
     @browser.option(text: /^#{user2}+/).select
     submit
