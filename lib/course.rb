bca0122d67946fea781bb26603f15d48e9ad501d
Danny Smith <danny@dasmith.co.uk>
2015-04-08 11:42:02 +0100

    Added Course Class

---
+++ lib/course.rb
@@ -0,0 +1,10 @@
+# Object Representing Coures in the Moddle Interface
+# -----------------------------------------------------------------------------
+# Authors: Nini Jewootah (njewootah@spartaglobal.com) & Danny Smith (dsmith@testingcircle.com)
+# Date Modified: 2015-04-08
+# -----------------------------------------------------------------------------
+
+class Course
+
+
+end
